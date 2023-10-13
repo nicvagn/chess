5bb0934418129ea6657c9040961cce1b5376aaa0
Nicolas Vaagen <nicolasvaagen@gmail.com>
2023-10-12 19:56:55 -0600

    Initial commit. Defined structure.

---
+++ src/Chess/Board.java
@@ -0,0 +1,5 @@
+package Chess;
+
+public class Board {
+	private Position[][] board = new Position[8][8];
+}
---
+++ src/Chess/Position.java
@@ -0,0 +1,9 @@
+package Chess;
+
+public class Position {
+    public boolean occupied = false;
+    private Piece piece;
+    private String position;
+
+
+}
